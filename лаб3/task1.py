b5ca23da80fb58ceeca72ce962ccf604ab37db2a
pechencka26 <114876085+pechencka26@users.noreply.github.com>
2023-10-26 00:56:09 +0300

    lab3

---
+++ лаб3/task1.py
@@ -0,0 +1,16 @@
+
+def find_index(items_list,find_item):
+    if find_item in items_list:
+        return items_list.index(find_item)
+
+
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list,find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
